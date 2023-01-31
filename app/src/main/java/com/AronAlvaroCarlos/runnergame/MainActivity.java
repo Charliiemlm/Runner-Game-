e59f89542f9a1b0c71a8405ad8616791615a7055
alvaro <alvaro@alvaro.alvaro>
2023-01-31 17:50:44 +0100

    hey actualizado

--- app/src/main/java/com/AronAlvaroCarlos/runnergame/MainActivity.java
+++ app/src/main/java/com/AronAlvaroCarlos/runnergame/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button boton3;
         Button boton2;
         Button boton;
     }
