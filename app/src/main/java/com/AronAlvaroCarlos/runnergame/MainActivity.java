7411fbd62646cf6bb5f87f06dcf0b5029c242290
Asura <mangomanaron@gmail.com>
2023-01-31 17:54:02 +0100

    He añadido otro Button

--- app/src/main/java/com/AronAlvaroCarlos/runnergame/MainActivity.java
+++ app/src/main/java/com/AronAlvaroCarlos/runnergame/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button boton9;
         Button boton3;
         Button boton2;
         Button boton;
