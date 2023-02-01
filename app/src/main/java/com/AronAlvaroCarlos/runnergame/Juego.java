560fe610782f73b9032dd9b3e20b48a34fb83a03
alvaro <alvaro@alvaro.alvaro>
2023-02-01 18:39:03 +0100

    clase juego y alguna cosa más

---
+++ app/src/main/java/com/AronAlvaroCarlos/runnergame/Juego.java
@@ -0,0 +1,14 @@
+package com.AronAlvaroCarlos.runnergame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Juego extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego);
+    }
+}
--- app/src/main/java/com/AronAlvaroCarlos/runnergame/MainActivity.java
+++ app/src/main/java/com/AronAlvaroCarlos/runnergame/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     private Button bSalir;
 
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
             lanzarPreferencias(null);
         }
     });
-		bPreferencias.setOnClickListener(new OnClickListener() {
+        bSalir.setOnClickListener(new OnClickListener() {
 
         @Override
         public void onClick(View v) {
@@ -79,27 +78,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
+       // getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
-        switch (item.getItemId()) {
-            case R.id.acercaDe:
-                lanzarAcercaDe(null);
-                break;
-            case R.id.config:
-                lanzarPreferencias(null);
-                break;
-        }
-        return true;
-    }
+
 
 
     public void lanzarPreferencias(View view) {
-        Intent i = new Intent(this, Preferencias.class);
+       // Intent i = new Intent(this, Preferencias.class);
         startActivity(i);
     }
 
